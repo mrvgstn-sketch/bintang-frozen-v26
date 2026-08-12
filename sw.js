47b1c9d910524d1502ca71b9bb716b3cce9a8ce8
mrvgstn-sketch <mrvgstn@gmail.com>
2026-08-12 07:24:00 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='bf-v27-1';
-const ASSETS=['./','./index.html','./manifest.json','./assets/css/app.css','./assets/icons/icon.svg','./js/db.js','./js/utils.js','./js/app.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
