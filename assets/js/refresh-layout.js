4acc585dc9f17da6191c1df4e11daca61907afab
mrvgstn-sketch <mrvgstn@gmail.com>
2026-08-14 02:53:04 +0700

    Delete assets directory

--- assets/js/refresh-layout.js
+++
@@ -1,7 +0,0 @@
-(function(){
-"use strict";
-function refreshSafeLayout(){document.querySelectorAll(".bf-final-menu-pop.bf-show").forEach(x=>x.classList.remove("bf-show"));document.querySelectorAll(".bf-final-menu-btn.bf-open").forEach(x=>x.classList.remove("bf-open"));}
-window.addEventListener("pageshow",refreshSafeLayout);
-window.addEventListener("orientationchange",refreshSafeLayout);
-window.addEventListener("resize",refreshSafeLayout);
-})();
