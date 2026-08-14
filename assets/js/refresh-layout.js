fc2c1983bf0203bb9d1a6d47f83b4d60cf8f3588
mrvgstn-sketch <mrvgstn@gmail.com>
2026-08-14 15:18:10 +0700

    Delete assets directory

--- assets/js/refresh-layout.js
+++
@@ -1,5 +0,0 @@
-(function(){
-"use strict";
-function closeTransient(){document.querySelectorAll(".bf-app-menu-pop.bf-show").forEach(x=>x.classList.remove("bf-show"));}
-window.addEventListener("pageshow",closeTransient);
-})();
